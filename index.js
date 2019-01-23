1b56cdf7e8879f58da47817c36c7137d58fa8b75
Brad McKinney <brad.mckin@gmail.com>
2019-01-23 09:33:25 -1000

    creates a function to locate a value inside of a multidimensional array

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 function locate(array, target) {
-  // do work here
+  let located = false;
+  let checkStack = [];
+  checkStack.push(array);
+
+  while (checkStack.length > 0) {
+    let next = checkStack.pop();
+    next.forEach(item => {
+      if (item === target) {
+        located = true;
+      } else if (Array.isArray(item)) {
+        checkStack.push(item);
+      }
+    });
+  }
+  if (located === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = locate;
